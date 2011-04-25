a2ee1a7b22251a9e4ebb60664636ccbdb1ba10ff
Chetan Surpur <chetan51@gmail.com>
2011-04-24 21:07:50 -0700

    Cleaned up agendas templates

--- agendas/views.py
+++ agendas/views.py
@@ -3,6 +3,6 @@ from django.http import Http404
 from django.template import Context, loader
 
 def index(request):
-    template = loader.get_template('agenda_detail.html')
+    template = loader.get_template('base_agendas.html')
     context = Context()
     return HttpResponse(template.render(context))
