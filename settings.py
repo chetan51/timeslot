4de9bba780766b85375cb04bc08362132f5598d3
Chetan Surpur <chetan51@gmail.com>
2011-04-25 10:05:19 -0700

    Restored DEBUG and COMPRESS settings

--- settings.py
+++ settings.py
@@ -7,7 +7,6 @@ DJANGO_ROOT = os.path.dirname(os.path.realpath(django.__file__))
 SITE_ROOT = os.path.dirname(os.path.realpath(__file__))
 
 DEBUG = True
-COMPRESS = True
 TEMPLATE_DEBUG = DEBUG
 
 ADMINS = (
